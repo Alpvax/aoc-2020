c19d6de3ec5df2b371117093d09e04308b54bf7a
Alpvax <alpvax@netscape.net>
2020-12-07 16:28:06 +0000

    Cargo fmt

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -5,7 +5,7 @@ use std::iter::FromIterator;
 fn main() {
     let mut total1: usize = 0;
     let mut total2: usize = 0;
-    let mut current1 =  HashSet::new();
+    let mut current1 = HashSet::new();
     let mut current2: Option<HashSet<char>> = None;
     for line in read_lines("puzzle-input/day6.txt") {
         if line == "" {
@@ -22,14 +22,18 @@ fn main() {
             match current2 {
                 None => {
                     current2 = Some(HashSet::from_iter(line.chars()));
-                },
+                }
                 Some(ref mut set) => {
                     (*set).retain(|&c| line.contains(c));
-                },
+                }
             }
         }
     }
     total1 += current1.len();
-    total2 += if let Some(set) = current2 { set.len() } else { 0 };
+    total2 += if let Some(set) = current2 {
+        set.len()
+    } else {
+        0
+    };
     println!("Part 1: {}\nPart 2: {}", total1, total2);
 }
