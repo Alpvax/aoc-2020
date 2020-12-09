f9f2007315e556b408f67b80a7a0e62580ee3f65
Alpvax <alpvax@netscape.net>
2020-12-09 10:30:59 +0000

    Complete day9.2

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -9,13 +9,14 @@ fn main() {
     for _ in 0..CHECK_LENGTH {
         saved.push(iter.next().unwrap());
     }
+    let mut result = 0;
     for num in iter {
         saved.push(num);
-        if !check(&saved) {
-            println!("Part 1: {}", num);
-            break;
+        if result == 0 && !check(&saved) {
+            result = num;
         }
     }
+    println!("Part 1: {}\nPart 2: {}", result, part2(&saved, result));
 }
 
 fn check(numbers: &Vec<u64>) -> bool {
@@ -32,4 +33,23 @@ fn check(numbers: &Vec<u64>) -> bool {
         }
     }
     false
+}
+
+fn part2(numbers: &Vec<u64>, target: u64) -> u64 {
+    for (i, num) in numbers.iter().enumerate() {
+        let mut total = *num;
+        for &n in numbers[(i + 1)..].iter() {
+            if n < target {
+                total += n;
+                if total < target {
+                    continue;
+                }
+                if total == target {
+                    return num + n;
+                }
+            }
+            break;
+        }
+    }
+    panic!("No contigous region found!");
 }
